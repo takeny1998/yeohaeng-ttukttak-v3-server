00f7e29a8823e2296c79bf48838bc85f711a6f29
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-02-22 15:07:01 +0900

    feat: 샘플 엔드포인트 추가

---
+++ src/main/java/com/yeohaeng_ttukttak/server/application/HelloController.java
@@ -0,0 +1,16 @@
+package com.yeohaeng_ttukttak.server.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v3/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+
+}
