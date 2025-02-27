55d90b2324a34f339e4be86e7800c8e64994dc9b
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-02-27 19:27:18 +0900

    RegionCode 동등성 테스트 삭제
    - 동등성을 `Repository`에서 직접 비교하면서, `equals`를 구현하는 의미기 없어짐. 그러므로 해당 테스트를 삭제했음

--- src/test/java/com/yeohaeng_ttukttak/server/domain/place/RegionCodeTest.java
+++ src/test/java/com/yeohaeng_ttukttak/server/domain/place/RegionCodeTest.java
@@ -2,8 +2,6 @@ package com.yeohaeng_ttukttak.server.domain.place;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RegionCodeTest {
@@ -21,34 +19,6 @@ class RegionCodeTest {
         assertThat(regionCode)
                 .extracting(RegionCode::getLevel1, RegionCode::getLevel2, RegionCode::getLevel3, RegionCode::getLevel4)
                 .containsExactly(29, 155, 10, 0);
-    }
-
-    @Test
-    void equalsTest() {
-
-        final List<RegionCode> regionCodes = List.of(
-                new RegionCode(21, 10, 0, 0),  // 0: 21 010 000 00
-                new RegionCode(21, 10, 0, 0),  // 1: 21 010 000 00
-                new RegionCode(21, 20, 0, 0),  // 2: 21 020 000 00
-                new RegionCode(21, 10, 30, 0), // 3: 21 010 030 00
-                new RegionCode(23, 10, 0, 0)   // 4: 23 010 000 00
-        );
-
-        assertThat(regionCodes.get(0))
-                .as("모든 Level이 같으면 동등해야 한다.")
-                .isEqualTo(regionCodes.get(1));
-
-        assertThat(regionCodes.get(0))
-                .as("한 개라도 Level이 틀리면 동등하지 않다.")
-                .isNotEqualTo(regionCodes.get(4));
-
-        assertThat(regionCodes.get(0))
-                .as("한 개라도 Level이 틀리면 동등하지 않다.")
-                .isNotEqualTo(regionCodes.get(2));
-
-        assertThat(regionCodes.get(0))
-                .as("level3가 다르지만, [3]의 level3이 0이므로 동등하다.")
-                .isEqualTo(regionCodes.get(3));
     }
 
 }
