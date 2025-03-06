b7fd25b323b5153a4c4380a82506b9b7b495848a
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2025-03-06 13:42:44 +0900

    feat: `RegionCode` 레벨 변환, 직렬화, 제약조건 추가
    - 하위 레벨을 가지는 새 엔티티를 반환하는 `RegionCode.withLowerLevel()` 구현
    - level 값을 기반으로 직렬화를 수행하는 `serialize()` 구현
    - 관련 테스트 코드 작성
    - `RegionCode` 에 제약조건 추가

--- src/main/java/com/yeohaeng_ttukttak/server/domain/region/Region.java
+++ src/main/java/com/yeohaeng_ttukttak/server/domain/region/Region.java
@@ -1,27 +1,53 @@
 package com.yeohaeng_ttukttak.server.domain.region;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
+import lombok.*;
 import org.hibernate.annotations.Immutable;
 
 @Entity
 @Immutable
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Region {
 
     @Id
     private Long id;
 
-    private int level;
-
     private String name;
 
-    @Embedded @NotNull
+    @Embedded
+    @NotNull
     private RegionCode code;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Region parent;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    Region(Long id,  String name, RegionCode code, Region parent) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.parent = parent;
+    }
+
+    /**
+     * 상위 지역과 콤마(,)로 구분된 지역 이름을 반환합니다.
+     *
+     * @return 상위 지역이 있으면 {@code 서울특별시, 대한민국}; 없으면 {@code 대한민국}
+     */
+    public String serialize() {
+        final StringBuilder nameBuilder = new StringBuilder();
+
+        nameBuilder.append(getName());
+
+        if (this.getParent() != null) {
+            nameBuilder.append(", ").append(getParent().getName());
+        }
+
+        return nameBuilder.toString();
+    }
 
 }
--- src/test/java/com/yeohaeng_ttukttak/server/domain/region/RegionCodeTest.java
+++ src/test/java/com/yeohaeng_ttukttak/server/domain/region/RegionCodeTest.java
@@ -1,6 +1,5 @@
 package com.yeohaeng_ttukttak.server.domain.region;
 
-import com.yeohaeng_ttukttak.server.domain.region.RegionCode;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +19,46 @@ class RegionCodeTest {
         assertThat(regionCode)
                 .extracting(RegionCode::getLevel1, RegionCode::getLevel2, RegionCode::getLevel3, RegionCode::getLevel4)
                 .containsExactly(29, 155, 10, 0);
+    }
+
+    @Test
+    void withLowerLevelTest() {
+        // Given
+        final RegionCode regionCode = RegionCode.builder()
+                .level(4).level1(11).level2(110).level3(103).build();
+
+        final RegionCode codeLevel1 = RegionCode.builder()
+                .level(1).build();
+
+        final RegionCode codeLevel2 = RegionCode.builder()
+                .level(2).level1(11).build();
+
+        final RegionCode codeLevel3 = RegionCode.builder()
+                .level(3).level1(11).level2(110).build();
+
+        // When
+        final RegionCode withLevel1 = RegionCode.withLowerLevel(regionCode, 1);
+        final RegionCode withLevel2 = RegionCode.withLowerLevel(regionCode, 2);
+        final RegionCode withLevel3 = RegionCode.withLowerLevel(regionCode, 3);
+
+        // Then
+        assertThat(withLevel1).isEqualTo(codeLevel1);
+        assertThat(withLevel2).isEqualTo(codeLevel2);
+        assertThat(withLevel3).isEqualTo(codeLevel3);
+    }
+
+    @Test
+    void serializeTest() {
+        // Given
+        final RegionCode regionCode =RegionCode.builder()
+                .level1(11).level2(110).level3(103).build();
+        final String expectString = "1111010300";
+
+        // When
+        final String string = regionCode.serialize();
+
+        // Then
+        assertThat(string).isEqualTo(expectString);
     }
 
 }
